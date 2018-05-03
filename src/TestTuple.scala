52fbafa574dd313e04f35f428baed89d6a3b226c
ezzheyi <yinjie.zheng@ericsson.com>
2018-05-03 15:04:29 +0800

    tuple的遍历

--- src/TestTuple.scala
+++ src/TestTuple.scala
@@ -1,19 +1,9 @@
 object TestTuple {
   def main(args: Array[String]): Unit = {
-    val re = List("hello world hello scala")
-      .flatMap(_.split(" "))
-      .map((_,1))
-      .groupBy(_._1)
+    val t1 = (1, "2", 'a', true)
 
-      .map(t => {
-        var size = 0
-        t._2.map( t => {
-          size += t._2
-        })
-        (t._1,size)
-      })
-      .toList.sortBy(_._2).reverse.toBuffer
-    println(re)
-
+    for(e <- t1.productIterator) {
+      println(e)
+    }
   }
 }
